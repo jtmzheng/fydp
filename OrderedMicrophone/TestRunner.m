588f726329018d61f1b404e8653c7f27c37c26b9
jtmzheng <jtmzheng@gmail.com>
2016-11-01 20:01:08 -0400

    Added in error constant to F1, F2 (ideal relative delays)
    Also changed angle range for sim to [0, 240] until fix equations to work
    [0, 360]

--- OrderedMicrophone/TestRunner.m
+++ OrderedMicrophone/TestRunner.m
@@ -3,7 +3,7 @@ clear; clc;
 
 % Test data
 r = linspace(0.3, 10, 10);
-theta = degtorad(linspace(0, 360, 36));
+theta = degtorad(linspace(0, 240, 36));
 
 % Generate every test combination of r, theta
 [R, Theta] = meshgrid(r, theta);
@@ -11,13 +11,15 @@ theta = degtorad(linspace(0, 360, 36));
 % Constants
 l = 0.3;                % distance between microphones
 ss = 300;               % speed of sound (m/s)
+err = 0.001;
 
 [F1, F2] = calcRelativeDelay(R, Theta, l);
 res = zeros(size(R));
+
 for i=1:size(R,1)
     for k=1:size(R,2)
         init_val = [5, pi/6];
-        f = @(x)delayFunc(x, F1(i, k), F2(i, k), l);
+        f = @(x)delayFunc(x, F1(i, k) + err, F2(i, k) + err, l);
         result = fsolve(f, init_val);
         res(i, k) = result(2);  % only keep angle
     end
@@ -25,4 +27,4 @@ end
 
 %% Plotting
 hold on;
-mesh(R, Theta, radtodeg(res - Theta));
+mesh(R, radtodeg(Theta), radtodeg(res - Theta));
