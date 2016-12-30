afd50fe906118fdb3126a76f22b7bb89a9b2ae97
bslin <bill.s.lin1@gmail.com>
2016-12-30 18:05:30 -0500

    Revert "Revert "Added fake server on linux in comm""
    This reverts commit 12d0667c22909313e751b9f77017ca613d22908b.

--- PruMem/commUtils.h
+++ PruMem/commUtils.h
@@ -1,16 +1,32 @@
 #ifndef COMM_UTILS_H
 #define COMM_UTILS_H
 
-#include "dataBuffer.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdint.h>
+
+#define RING_BUFFER_SIZE    (8388576)
+#define MAX(X, Y)           (((X) > (Y)) ? (X) : (Y))
+#define MIN(X, Y)           (((X) < (Y)) ? (X) : (Y))
 
 typedef struct comm_handle_s
 {
     int mSockFd;
     
     int mNewSocketFd; // Socket for each client connection
-    size_t mNumOutputSamples;
+    int mNumOutputSamples;
 
 } comm_handle_t;
+
+// Data Buffer contains 2 pointers and sizes
+// This is to deal with the wrap around in ring buffer
+typedef struct data_buffer_s
+{
+    uint8_t *mData[2];
+    int mBufferSize[2];
+} data_buffer_t;
 
 int start_server(comm_handle_t *pCommHandle, int aPortNo);
 
