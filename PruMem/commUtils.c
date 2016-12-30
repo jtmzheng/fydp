7a56054f82dc217aee03adc0796126a4812de94b
bslin <bill.s.lin1@gmail.com>
2016-12-30 08:49:20 -0500

    Added fake server on linux in comm

--- PruMem/commUtils.c
+++ PruMem/commUtils.c
@@ -19,7 +19,7 @@ int read_data(int aNewSockFd, char *aBuffer, int aBytesExpected)
 
         if (n < 0)
         {
-            error("ERROR reading from socket");
+            printf("ERROR reading from socket");
             ret = -1;
         }
         else
@@ -43,7 +43,7 @@ int write_data(int aNewSockFd, char *aBuffer, int aBytesExpected)
 
         if (n < 0)
         {
-            error("ERROR writing to socket");
+            printf("ERROR writing to socket");
             ret = -1;
         }
         else
@@ -66,7 +66,7 @@ int start_server(comm_handle_t *pCommHandle, int aPortNo)
     pCommHandle->mSockFd = socket(AF_INET, SOCK_STREAM, 0);
     if (pCommHandle->mSockFd < 0)
     {
-        error("ERROR opening socket");
+        printf("ERROR opening socket");
         ret = -1;
     }
 
@@ -80,7 +80,7 @@ int start_server(comm_handle_t *pCommHandle, int aPortNo)
                  (struct sockaddr *) &serv_addr,
                  sizeof(serv_addr)) < 0) 
         {
-            error("ERROR on binding");
+            printf("ERROR on binding");
             ret = -1;
         }
     }
@@ -95,7 +95,7 @@ int get_request(comm_handle_t *pCommHandle)
     socklen_t clilen;
     char buffer[256];
     struct sockaddr_in cli_addr;
-    size_t numberOutputSamples;
+    int numberOutputSamples;
 
     // Get new client connection
     listen(pCommHandle->mSockFd, 5);
@@ -110,7 +110,7 @@ int get_request(comm_handle_t *pCommHandle)
 
     if (newsockfd < 0)
     {
-        error("ERROR on accept");
+        printf("ERROR on accept");
         ret = -1;
     }
 
@@ -141,31 +141,25 @@ int get_request(comm_handle_t *pCommHandle)
 int send_request(comm_handle_t *pCommHandle, data_buffer_t *pMappedDataBuffer)
 {
     int ret = 0;
-    size_t num_samples_to_send = pMappedDataBuffer->mBufferSize[0] + pMappedDataBuffer->mBufferSize[1];
-    size_t num_samples_to_send_nt;
+    int num_samples_to_send = pMappedDataBuffer->mBufferSize[0] + pMappedDataBuffer->mBufferSize[1];
+    int num_samples_to_send_nt;
     num_samples_to_send = MIN(pCommHandle->mNumOutputSamples, num_samples_to_send);
     num_samples_to_send_nt = htonl(num_samples_to_send);
 
-    printf("a\n");
+    printf("Sending Data Size\n");
     ret = write_data(pCommHandle->mNewSocketFd, (char*)&num_samples_to_send_nt, 4);
-    printf("b\n");
     if (ret == 0 && num_samples_to_send > 0)
     {
-        printf("c\n");
-        size_t num_sent_data = MIN(pMappedDataBuffer->mBufferSize[0], num_samples_to_send);
+        int num_sent_data = MIN(pMappedDataBuffer->mBufferSize[0], num_samples_to_send);
         ret = write_data(pCommHandle->mNewSocketFd, (char*)pMappedDataBuffer->mData[0], num_sent_data);
         num_samples_to_send -= num_sent_data;
-        printf("d\n");
     }
 
     if (ret == 0 && num_samples_to_send > 0)
     {
-        printf("e\n");
-        size_t num_sent_data = MIN(pMappedDataBuffer->mBufferSize[1], num_samples_to_send);
+        int num_sent_data = MIN(pMappedDataBuffer->mBufferSize[1], num_samples_to_send);
         ret = write_data(pCommHandle->mNewSocketFd, (char*)pMappedDataBuffer->mData[1], num_sent_data);
-        printf("f\n");
     }
-    printf("g\n");
     close(pCommHandle->mNewSocketFd);
     pCommHandle->mNewSocketFd = 0;
     pCommHandle->mNumOutputSamples = 0;
