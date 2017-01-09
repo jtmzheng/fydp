6c3d1c83a068087b841f99cad050e9b402bd9dd0
Bill S Lin <bslin@uwaterloo.ca>
2017-01-09 18:22:16 -0500

    Monitoring system with callback function (#2)
    * Monitoring system with callback function
    
    * Added necessary comments.
    Fixed bug (starting stream only once in a while)
    
    * Changed callback as a single function again

---
+++ Monitor/monitor.py
@@ -0,0 +1,60 @@
+from sys import byteorder
+from array import array
+from numpy import mean, sqrt, square
+
+import pyaudio
+import time
+
+CHUNK_SIZE = 512
+FORMAT = pyaudio.paInt16
+RATE = 44100
+
+def is_silent(snd_data, threshold):
+    """Returns 'True' if below the 'silent' threshold"""
+    sound_rms = sqrt(mean(square(snd_data)))
+    print("RMS: %d\n" % sound_rms)
+    return sound_rms < threshold
+
+def tmp_callback():
+    print ("Callback!\n")
+    time.sleep(1)
+    print ("Resuming!\n")
+
+def monitor_sound(callback, threshold):
+    """
+    Go to Infinite Monitoring Loop
+    Note: We close the old audio stream and create a new one each time
+    we capture noise to ensure we don't process stale data that was left in the
+    old stream
+    """
+
+    p = pyaudio.PyAudio()
+    
+    while 1:
+        stream = p.open(format=FORMAT, channels=1, rate=RATE,
+                        input=True, output=True,
+                        frames_per_buffer=CHUNK_SIZE)
+
+        silent = True
+
+        # Keep polling until silent
+        while (silent):
+            # little endian, signed short
+            snd_data = array('h', stream.read(CHUNK_SIZE))
+            if byteorder == 'big':
+                snd_data.byteswap()
+
+            silent = is_silent(snd_data, threshold)
+
+        stream.stop_stream()
+        callback()
+
+        # We should close the audio stream and create
+        # a new one to ensure we don't look at new data
+        stream.close()
+    p.terminate()
+
+
+if __name__ == '__main__':
+    print("Starting Monitoring System!\n")
+    monitor_sound(tmp_callback, 10000)
