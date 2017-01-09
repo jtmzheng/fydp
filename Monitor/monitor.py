d82029daeab51a900367dfb0acfafa4aba30ed54
bslin <bill.s.lin1@gmail.com>
2017-01-08 20:24:05 -0500

    Added necessary comments. Fixed bug (starting stream only once in a while)

--- Monitor/monitor.py
+++ Monitor/monitor.py
@@ -5,27 +5,29 @@ from numpy import mean, sqrt, square
 import pyaudio
 import time
 
-THRESHOLD = 10000
 CHUNK_SIZE = 512
 FORMAT = pyaudio.paInt16
 RATE = 44100
 
-def is_silent(snd_data):
-    # Returns 'True' if below the 'silent' threshold
+def is_silent(snd_data, threshold):
+    """Returns 'True' if below the 'silent' threshold"""
     sound_rms = sqrt(mean(square(snd_data)))
     print("RMS: %d\n" % sound_rms)
-    return sound_rms < THRESHOLD
+    return sound_rms < threshold
 
 def tmp_callback():
     print ("Callback!\n")
     time.sleep(1)
     print ("Resuming!\n")
 
-def monitor_sound(callback):
+def monitor_sound(callback_list, threshold):
+    """
+    Go to Infinite Monitoring Loop
+    Note: We close the old audio stream and create a new one each time
+    we capture noise to ensure we don't process stale data that was left in the
+    old stream
+    """
 
-    # Go to Infinite Monitoring Loop
-    # Note: We create a new audio stream
-    # every time so that 
     p = pyaudio.PyAudio()
     
     while 1:
@@ -33,24 +35,27 @@ def monitor_sound(callback):
                         input=True, output=True,
                         frames_per_buffer=CHUNK_SIZE)
 
-        # little endian, signed short
-        snd_data = array('h', stream.read(CHUNK_SIZE))
-        if byteorder == 'big':
-            snd_data.byteswap()
+        silent = True
 
-        silent = is_silent(snd_data)
+        # Keep polling until silent
+        while (silent):
+            # little endian, signed short
+            snd_data = array('h', stream.read(CHUNK_SIZE))
+            if byteorder == 'big':
+                snd_data.byteswap()
 
-        if not silent:
+            silent = is_silent(snd_data, threshold)
+
+        stream.stop_stream()
+        for callback in callback_list:
             callback()
 
-        # We should close the audio
-        # stream so the next time
-        sample_width = p.get_sample_size(FORMAT)
-        stream.stop_stream()
+        # We should close the audio stream and create
+        # a new one to ensure we don't look at new data
         stream.close()
     p.terminate()
 
 
 if __name__ == '__main__':
     print("Starting Monitoring System!\n")
-    monitor_sound(tmp_callback)
+    monitor_sound([tmp_callback], 10000)
