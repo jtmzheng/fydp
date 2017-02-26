b23203c859762ced5098b5dcc99b64657f11d77a
Max Zheng <jtmzheng@users.noreply.github.com>
2017-02-26 18:18:18 -0500

    Implementing xcorr using n-peaks

--- client/locate.py
+++ client/locate.py
@@ -6,13 +6,45 @@ from scipy.optimize import fsolve
 from scipy import signal
 
 import math
+import peakutils
 
 SPEED_SOUND = 300.0 # [m]
-SAMPLING_FREQ = 9500000.0 # [Hz]
+SAMPLING_FREQ = 9.6e6 # [Hz]
 
 # Max filter window size
 MED_WINDOW_SIZE = 21
 WINDOW_SIZE = 5000
+
+# Butterworth filter (manually calibrated) parameters
+FREQ_1 = 250 #Hz
+FREQ_2 = 300 #Hz
+
+# Number of peaks we want to window signal over
+N_PEAKS = 10
+PEAK_WINDOW_PREFIX = 3*36000
+PEAK_WINDOW_SUFFIX = 36000
+
+def apply_butter(f1, f2, fs, sig):
+    """ Apply second order Butterworth filter to sig
+    fs is sampling rate
+    """
+    sos = sp.signal.butter(
+        2, (np.array([f1, f2]) / (fs / 2)), btype='bandpass', analog=False, output='sos'
+    )
+    sig = signal.sosfiltfilt(sos, sig)
+    return sig
+
+def normalize_signal(sig):
+    """ Normalize signal and zero-mean
+    """
+    sig = (sig - np.min(sig)) / (np.max(sig) - np.min(sig))
+    return sig - np.mean(sig)
+
+def get_n_peaks(sig, thres, min_dist, n):
+    """ Gets the first n peaks > thres ([0, 1]) and greater than min_dist apart
+    """
+    idx = peakutils.peak.indexes(sig, thres=thres, min_dist=min_dist)
+    return idx[:n]
 
 def movmax(seq, window=1):
     """Moving windowed max over seq of input window size
@@ -64,15 +96,47 @@ def locate(f1, f2, l, r0=5, theta0=(math.pi/6)):
     r_hat, theta_hat = fsolve(func, [r0, theta0])
     return r_hat, theta_hat
 
-def xcorr(sig1, sig2):
-    """ Cross-correlation (NB: http://stackoverflow.com/questions/12323959/fast-cross-correlation-method-in-python)
+def find_peak_window(sig, thres, min_dist, n):
+    """ Crop `sig` around n peaks using a Butterworth filter to smooth peaks
     """
+    # Butterworth filter (easier to get peaks)
+    sig_butter = normalize_signal(apply_butter(FREQ_1, FREQ_2, SAMPLING_FREQ, sig))
+
+    # Find indexes for first N peaks over threshold
+    idx = get_n_peaks(sig_butter, thres=thres, min_dist=min_dist, n=N_PEAKS)
+
+    # Window the signal
+    sig_butter[:idx[0]-PEAK_WINDOW_PREFIX] = 0
+    sig_butter[idx[-1]+PEAK_WINDOW_SUFFIX:] = 0
+    sig[:idx[0]-PEAK_WINDOW_PREFIX] = 0
+    sig[idx[-1]+PEAK_WINDOW_SUFFIX:] = 0
+    return sig, sig_butter
+
+def xcorr_peaks(sig1, sig2, n=N_PEAKS):
+    """ Compute cross-correlation after applying a Buttersworth filter (see IPython notebook) to find
+    first N peaks (crop around these peaks)
+    """
+    # Median filter both signals
     sig1 = signal.medfilt(sig1, MED_WINDOW_SIZE)
     sig2 = signal.medfilt(sig2, MED_WINDOW_SIZE)
 
+    # Zero mean
+    sig1 = sig1 - np.mean(sig1)
+    sig2 = sig2 - np.mean(sig2)
+
+    # Crop each signal about peaks
+    sig1_cropped, _ = find_peak_window(sig1, thres=0.6, min_dist=1000, n=n)
+    sig2_cropped, _ = find_peak_window(sig2, thres=0.6, min_dist=1000, n=n)
+
+    # Compute xcorr of the cropped signals
+    return xcorr(sig1_cropped, sig2_cropped)
+
+def xcorr(sig1, sig2):
+    """ Cross-correlation (NB: http://stackoverflow.com/questions/12323959/fast-cross-correlation-method-in-python)
+    """
     n = len(sig1)
-    sig1 = np.abs(sig1 - np.mean(sig1))
-    sig2 = np.abs(sig2 - np.mean(sig2))
+    sig1 = (sig1 - np.mean(sig1))
+    sig2 = (sig2 - np.mean(sig2))
 
     sig1 = movmax(sig1, WINDOW_SIZE)
     sig2 = movmax(sig2, WINDOW_SIZE)
