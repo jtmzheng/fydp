40c4b7985f3869922bd167c55dc1713fe44a909e
Max Zheng <jtmzheng@users.noreply.github.com>
2017-03-16 18:51:39 -0400

    Updating color scheme
    - Replacing `Quit` button with `Clear` (clears history)

--- client/gui.py
+++ client/gui.py
@@ -51,6 +51,15 @@ class GUI(threading.Thread):
         self.cur_mic_0 = None
         self.cur_mic_1 = None
 
+    def clear_history(self):
+        self.prev_mic_0 = []
+        self.prev_mic_1 = []
+
+        self.C.delete('line')
+        self.line_0 = self.C.create_line(self.cur_mic_0, fill='#648FB1', width=5, arrow = Tkinter.LAST, smooth=True, tag='line')
+        if num_mics == 2:
+            self.line_1 = self.C.create_line(self.cur_mic_1, fill='#648FB1', width=5, arrow = Tkinter.LAST, smooth=True, tag='line')
+
     def draw_mics(self, A0, A1):
         #  import MIC symbol
         print('----')
@@ -75,11 +84,11 @@ class GUI(threading.Thread):
 
         # Mic 0 Line
         self.cur_mic_0 = baseWidth0, baseHeight0, baseWidth0 + radius * math.sin(math.radians(A0)), baseHeight0 - radius * math.cos(math.radians(A0))
-        self.line_0 = self.C.create_line(self.cur_mic_0, fill='#FFE800', width=5, arrow = Tkinter.LAST, smooth=True, tag='line')
+        self.line_0 = self.C.create_line(self.cur_mic_0, fill='#648FB1', width=5, arrow = Tkinter.LAST, smooth=True, tag='line')
         if num_mics == 2:
             # Mic 1 Line
             self.cur_mic_1 = baseWidth1, baseHeight1, baseWidth1 + radius * math.sin(math.radians(A1)), baseHeight1 - radius * math.cos(math.radians(A1))
-            self.line_1 = self.C.create_line(self.cur_mic_1, fill='#FFE800', width=5, arrow = Tkinter.LAST, smooth=True, tag='line')
+            self.line_1 = self.C.create_line(self.cur_mic_1, fill='#648FB1', width=5, arrow = Tkinter.LAST, smooth=True, tag='line')
 
         # Draw 'ghost' lines
         for mic in self.prev_mic_0:
@@ -130,15 +139,15 @@ class GUI(threading.Thread):
         self.top = Tkinter.Tk()
 
         # Canvas for main Vizulization
-        self.C = Tkinter.Canvas(self.top, bg='#7373D9', height=WINDOW_HEIGHT, width=WINDOW_WIDTH)
+        self.C = Tkinter.Canvas(self.top, bg='#333333', height=WINDOW_HEIGHT, width=WINDOW_WIDTH)
 
         # Mic 0 Line
         self.cur_mic_0 = baseWidth0, baseHeight0, baseWidth0 + radius * math.sin(math.radians(angle0)), baseHeight0 - radius * math.cos(math.radians(angle0))
-        self.line_0 = self.C.create_line(self.cur_mic_0, fill='#FFE800', width=5, arrow=Tkinter.LAST, smooth=True, tag='line')
+        self.line_0 = self.C.create_line(self.cur_mic_0, fill='#648FB1', width=5, arrow=Tkinter.LAST, smooth=True, tag='line')
 
         # Mic 1 Line
         self.cur_mic_1 = baseWidth1, baseHeight1, baseWidth1 + radius * math.sin(math.radians(angle1)), baseHeight1 - radius * math.cos(math.radians(angle1))
-        self.line_1 = self.C.create_line(self.cur_mic_1, fill='#FFE800', width = 5, arrow = Tkinter.LAST, smooth=True, tag='line')
+        self.line_1 = self.C.create_line(self.cur_mic_1, fill='#648FB1', width = 5, arrow = Tkinter.LAST, smooth=True, tag='line')
 
         micIcon = ImageTk.PhotoImage(image)
         A0 = self.C.create_image((baseWidth0, baseHeight0), image = micIcon,tag='mic')
@@ -156,34 +165,37 @@ class GUI(threading.Thread):
         # self.C.bind('<Configure>', self.create_grid)
 
         ####### Button Setup
-        MyButton1 = Button(self.top, text='Run', width=10, command=self.runMonitor, bg = 'white', relief=GROOVE)
+        MyButton1 = Button(self.top, text='Run', width=10, command=self.runMonitor, bg='white', relief=GROOVE)
         MyButton1.place(relx=0.25, rely=0.95, anchor='c')
 
         MyButton2 = Button(self.top, text='Update', width=10, command=lambda: self.update_arrays(self.DistanceEntry, self.NumArrayEntry, A0, A1), bg = 'white')
         MyButton2.place(relx=0.5, rely=0.95, anchor='c')
 
-        MyButton3 = Button(self.top, text='Quit', width=10, command=self.callback,bg = 'white')
-        MyButton3.place(relx=0.75, rely=0.95, anchor='c')
+        #MyButton3 = Button(self.top, text='Quit', width=10, command=self.callback,bg='white')
+        #MyButton3.place(relx=0.75, rely=0.95, anchor='c')
+
+        MyButton4 = Button(self.top, text='Clear', width=10, command=self.clear_history)
+        MyButton4.place(relx=0.75, rely=0.95, anchor='c')
 
         self.DistanceEntry = Tkinter.Entry(self.top,  width = 8)
         self.DistanceEntry.insert(END, '2')
         self.DistanceEntry.place(x=WINDOW_HEIGHT/4, y=WINDOW_HEIGHT/10*8.5, anchor='c')
 
-        DistanceText = Tkinter.Label(self.top, text='Array Distance', font='Avenir\bNext', background = '#090974', fg = 'white', height = 1)
+        DistanceText = Tkinter.Label(self.top, text='Array Distance', font='Avenir\bNext', background='#090974', fg='white', height=1)
         DistanceText.place(x=WINDOW_HEIGHT/4, y=WINDOW_HEIGHT/5 *4, anchor='c')
 
         self.NumArrayEntry = Tkinter.Entry(self.top, width = 8)
         self.NumArrayEntry.insert(END, '2')
         self.NumArrayEntry.place(x=WINDOW_HEIGHT/5*2.5, y=WINDOW_HEIGHT/10*8.5, anchor='c')
 
-        NumArrayText = Tkinter.Label(self.top, text='# Of Arrays', font='Avenir\bNext', background = '#090974', fg = 'white', height = 1)
+        NumArrayText = Tkinter.Label(self.top, text='# Of Arrays', font='Avenir\bNext', background='#090974', fg='white', height=1)
         NumArrayText.place(x=WINDOW_HEIGHT/2, y=WINDOW_HEIGHT/5 *4, anchor='c')
 
         self.NumRunEntry = Tkinter.Entry(self.top, width = 8)
         self.NumRunEntry.insert(END, '2')
         self.NumRunEntry.place(x=WINDOW_HEIGHT/4*3, y=WINDOW_HEIGHT/10*8.5, anchor='c')
 
-        NumRunText = Tkinter.Label(self.top, text='# of Runs', font='Avenir\bNext', background = '#090974', fg = 'white', height = 1)
+        NumRunText = Tkinter.Label(self.top, text='# of Runs', font='Avenir\bNext', background='#090974', fg='white', height=1)
         NumRunText.place(x=WINDOW_HEIGHT/4*3, y=WINDOW_HEIGHT/5 *4, anchor='c')
 
         self.top.mainloop()
